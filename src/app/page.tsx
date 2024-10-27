1d97dcc355080596ad7c38ece6ad0e735ea16ee7
beratungheilig <beratung.heilig@cloudmail.de>
2024-10-27 12:05:05 +0100

    Texterweiterung.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,24 @@ export default function Home() {
         </div>
       <ContentWrapper>
         <p>
-          In meiner psychodynamisch- systemischen Arbeit als Coach und Supervisorin bewege ich mich stets im Spannungsfeld zwischen unterstützender Begleitung und zielgerichteter Strukturierung. Mit einem feinen Gespür für die Bedürfnisse und Dynamiken moderner Arbeitskontexte hilft Supervision Klarheit zu schaffen und eigene Werte sowie eine authentische berufliche Haltung zu entwickeln.
+          In meiner psychodynamisch- systemischen Arbeit als Coach und Supervisorin bewege ich mich stets 
+          im Spannungsfeld zwischen unterstützender Begleitung und zielgerichteter Strukturierung. Mit einem 
+          feinen Gespür für die Komplexität und Dynamiken moderner Arbeitskontexte hilft Supervision Klarheit 
+          zu schaffen und eigene Werte sowie eine authentische berufliche Haltung zu entwickeln.
         </p>
         <br />
         <p>
-          Dabei ist es mir wichtig, dass der Beratungsprozess nicht nur Strukturen schafft, sondern es auch gelingt diese so zu verweben, dass diese den individuellen und kollektiven Interessen dienen.
+          Dabei ist es mir wichtig, dass der Beratungsprozess nicht nur Strukturen schafft, sondern es auch 
+          gelingt diese so zu verweben, dass diese den individuellen und kollektiven Interessen dienen.
         </p>
         <br />
         <p>
-          Meine Rolle als Supervisorin sehe ich darin, die Suche nach Lösungen und Erkenntnissen zu begleiten. Durch das Schaffen eines sicheren und unterstützenden Rahmens ermögliche ich es meinen Klienten, ihre Potentiale zu entdecken und zu entfalten. So fördere ich nicht nur persönliches Wachstum, sondern auch eine klare, gemeinsame Ausrichtung innerhalb von Teams.
+          Meine Rolle als Supervisorin sehe ich darin, die Suche nach Lösungen und Erkenntnissen zu begleiten. 
+          Der sicheren und unterstützenden Rahmen meiner Beratung ermöglicht einen Ort der Diskursfreundlichkeit, 
+          Reflexivität und Kreativität und fördert Emanzipation und Transparenz. Zudem sorge ich dafür 
+          klare Rollen und Verantwortlichkeiten in der Arbeitswelt zu definieren und damit auch ihre 
+          Potentiale zu entdecken und zu entfalten. So fördere ich nicht nur persönliches Wachstum, sondern auch 
+          eine klare, gemeinsame Ausrichtung innerhalb von Teams.
         </p>
         <br />
         <p>
